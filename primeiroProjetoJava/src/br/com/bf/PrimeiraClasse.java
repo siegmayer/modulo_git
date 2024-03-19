c23372be93807d2e9424ce5254366aa33fcd9265
siegmayer <160394545+siegmayer@users.noreply.github.com>
2024-03-19 20:41:11 -0300

    Add files via upload

---
+++ primeiroProjetoJava/src/br/com/bf/PrimeiraClasse.java
@@ -0,0 +1,15 @@
+package br.com.bf;
+
+public class PrimeiraClasse {
+
+	public static void main(String[] args) {
+		String ambiente = args [0];
+		// TODO Auto-generated method stub
+		if (ambiente.equals("DEV")) {
+	         System.out.print("DEV");
+
+		}
+
+    }
+
+}
